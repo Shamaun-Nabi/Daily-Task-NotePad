8083ea95b53564f845f33d51c2b052e26b64de4b
Shamaun-Nabi <snashik950@gmail.com>
2021-12-28 13:53:15 +0600

    design and animation added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="container d-flex align-items-center justify-content-center">
       <div className="mainWrapper d-flex align-items-center justify-content-center">
         <div className="displayWrapper">
-          <h3 className="text-center mb-2">My NotePad</h3>
+          <h3 className="text-center mb-2 tracking-in-expand">My NotePad</h3>
           <div className="row d-flex  justify-content-center">
             <div className=" col-sm-12 col-md-6 col-lg-6 leftBar d-none d-md-block d-lg-block">
               <lottie-player
@@ -24,38 +24,28 @@ function App() {
               <div className="contentWrapper d-flex align-items-center justify-content-center">
                 <input
                   type="email"
-                  className="form-control w-50 text-center"
+                  className="form-control w-75 text-center"
                   id="exampleFormControlInput1"
                   placeholder="Add Task Here"
                 />
-                <button className="btn btn-success ml-2">Add Task</button>
+                <button className="btn  addBtn ml-2">
+                  <span className="material-icons addIcon">add_box</span>
+                  {/* <span class="material-icons-two-tone">clear</span> */}
+                </button>
               </div>
               <div className=" mt-3 TaskName">
-                <ul className=" ">
-                  <div className="taskList ">
+                <ul className="taskGroup ">
+                  <div className="taskList">
                     <li className=" w-75 m-2 p-1 text-dark d-flex justify-content-between  align-items-center">
-                      <span>🟢 Hello task</span>
-                      <button className="btn btn-dark">❌</button>
-                    </li>
-                    <li className=" w-75 m-2 p-1 text-dark d-flex justify-content-between  align-items-center">
-                      <span>🟢 Hello task</span>
-                      <button className="btn btn-dark">❌</button>
-                    </li>
-                    <li className=" w-75 m-2 p-1 text-dark d-flex justify-content-between  align-items-center">
-                      <span>🟢 Hello task</span>
-                      <button className="btn btn-dark">❌</button>
-                    </li>
-                    <li className=" w-75 m-2 p-1 text-dark d-flex justify-content-between  align-items-center">
-                      <span>🟢 Hello task</span>
-                      <button className="btn btn-dark">❌</button>
-                    </li>
-                    <li className=" w-75 m-2 p-1 text-dark d-flex justify-content-between  align-items-center">
-                      <span>🟢 Hello task</span>
-                      <button className="btn btn-dark">❌</button>
-                    </li>
-                    <li className=" w-75 m-2 p-1 text-dark d-flex justify-content-between  align-items-center">
-                      <span>🟢 Hello task</span>
-                      <button className="btn btn-dark">❌</button>
+                      <div className="taskWithIcon d-flex justify-content-center  align-items-center">
+                        <span className="material-icons penIcon rotate-90-cw ">verified</span>
+                        <span className="ml-3">Hello task</span>
+                      </div>
+
+                      <button className="">
+                      
+                        <span class="material-icons dltIcon">delete</span>
+                      </button>
                     </li>
                   </div>
                 </ul>
