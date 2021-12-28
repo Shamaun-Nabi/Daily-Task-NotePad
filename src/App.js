eac008d88a9c24f2b9b18aa5a5ad36125bab98fe
Shamaun-Nabi <snashik950@gmail.com>
2021-12-28 18:00:17 +0600

    input filed bug fixed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import ListItem from "./components/ListItem";
+import { useState } from "react";
 // import { fab } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
+  const [inputValue, setinputValue] = useState("");
   return (
     // Remove this area and start your code
     <div className="container d-flex align-items-center justify-content-center">
       <div className="mainWrapper d-flex align-items-center justify-content-center">
         <div className="displayWrapper">
           <h4 className="text-center mb-2 tracking-in-expand">My NotePad</h4>
-          <h6 className="text-center text-muted">Developed & Designed By Shamaun Nabi</h6>
+          <h6 className="text-center text-muted">
+            Developed & Designed By Shamaun Nabi
+          </h6>
           <div className="row d-flex  justify-content-center mt-3">
             <div className=" col-sm-12 col-md-6 col-lg-6 leftBar d-none d-md-block d-lg-block">
               <lottie-player
@@ -23,18 +27,22 @@ function App() {
               ></lottie-player>
             </div>
             <div className="col-sm-12 col-md-6 col-lg-6 rightBar  ">
-              <div className="contentWrapper d-flex align-items-center justify-content-center">
+              <form className="contentWrapper d-flex align-items-center justify-content-center">
                 <input
                   type="email"
                   className="form-control w-75 text-center"
                   id="exampleFormControlInput1"
                   placeholder="Add Task Here"
+                  required
                 />
-                <button className="btn  addBtn ml-2">
-                  <span className="material-icons addIcon">add_box</span>
-                  {/* <span class="material-icons-two-tone">clear</span> */}
-                </button>
-              </div>
+                <input
+                  type="submit"
+                  value="Add"
+                  className="btn  text-white addBtn ml-2"
+                />
+                {/* <span className="material-icons addIcon">add_box</span> */}
+                {/* <span class="material-icons-two-tone">clear</span> */}
+              </form>
               <div className=" mt-3 TaskName">
                 <ul className="taskGroup ">
                   <div className="taskList">
